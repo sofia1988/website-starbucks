6c08171b146a1198fc7fb6f5ed06caa7972da804
sofia <brunila110@gmail.com>
2021-05-30 19:20:46 -0300

    media.css

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color ;
 }
+function toggleMenu() {
+         
+    var menuToggle= document.querySelector('.toggle');
+    var navigation=document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
