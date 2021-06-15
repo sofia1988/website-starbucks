da62881de3320f7a42be6d0476c670f155391643
Alicia Sofia Ruiz <62399971+sofia1988@users.noreply.github.com>
2021-06-15 00:33:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color ;
 }
+
+// const myMenu = document.getElementById("myMenu");
+// myMenu.onclick = toggleMenu;
+
+myMenu.addEventListener("click",toggleMenu)
+
+
 function toggleMenu() {
          
     var menuToggle= document.querySelector('.toggle');
